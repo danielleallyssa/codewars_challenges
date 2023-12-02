7e38cbc72fa6f5c7bdc01a488b9f346bc90b950c
Danielle Brown <hello@danielleallyssa.com>
2023-12-02 18:13:39 -0500

    Solution - Basic object

---
+++ 8kyu/basic-object.js
@@ -0,0 +1,8 @@
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+animal = (obj) => `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
