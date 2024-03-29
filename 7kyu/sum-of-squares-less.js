5b1b49d12ac43c20bd6f470005dc64d49e4f9391
Danielle Brown <hello@danielleallyssa.com>
2024-03-29 02:34:41 -0400

    Solution - Sum of squares less

---
+++ 7kyu/sum-of-squares-less.js
@@ -0,0 +1,15 @@
+// Write a function getNumberOfSquares (C, F#, Haskell) / get_number_of_squares (Python, Ruby) that will return how many integer (starting from 1, 2...) numbers raised to power of 2 and then summed up are less than some number given as a parameter.
+
+// E.g 1: For n = 6 result should be 2 because 1^2 + 2^2 = 1 + 4 = 5 and 5 < 6 E.g 2: For n = 15 result should be 3 because 1^2 + 2^2 + 3^2 = 1 + 4 + 9 = 14 and 14 < 15
+
+function getNumberOfSquares(n) {
+  let sum = 0,
+    total;
+
+  for (let i = 1; sum < n; i++) {
+    sum += i ** 2;
+    total = i - 1;
+  }
+
+  return total;
+}
