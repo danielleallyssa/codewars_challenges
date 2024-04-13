27838ce0d20fdbf608a59f16720bfa282e3b63dd
Danielle Brown <hello@danielleallyssa.com>
2024-04-13 18:46:56 -0400

    Solution - Find the stray

---
+++ 7kyu/find-the-stray.js
@@ -0,0 +1,8 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+const stray = (numbers) =>
+  numbers.sort((a, b) => a - b).find((el) => el !== numbers[1]);
