8b7cd085f1dd66857f04619191741c0579bc62f9
Danielle Brown <hello@danielleallyssa.com>
2024-04-18 18:47:54 -0400

    Solution - Multiple of five

---
+++ 7kyu/multiple-of-five.js
@@ -0,0 +1,16 @@
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+
+const roundToNext5 = (n) => Math.ceil(n / 5) * 5;
